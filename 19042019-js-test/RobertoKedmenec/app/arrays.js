2a1d9587a4270cd386c6fa48cad3ac8d57028920
Roberto Kedmenec <rkedmenec@tria.hr>
2019-04-24 10:43:28 +0200

    Added for ptomasic and rkedmenec.

---
+++ 19042019-js-test/RobertoKedmenec/app/arrays.js
@@ -0,0 +1,63 @@
+exports = typeof window === 'undefined' ? global : window;
+
+exports.arraysAnswers = {
+  indexOf: function (arr, item) {
+    return arr.indexOf(item);
+  },
+
+  sum: function (arr) {
+    return arr.reduce((prev, next) =>  prev + next)
+  },
+
+  remove: function (arr, item) {
+
+  },
+
+  removeWithoutCopy: function (arr, item) {
+
+    return arr;
+
+
+  },
+
+  append: function (arr, item) {
+
+
+
+  },
+
+  truncate: function (arr) {
+
+  },
+
+  prepend: function (arr, item) {
+
+  },
+
+  curtail: function (arr) {
+
+  },
+
+  concat: function (arr1, arr2) {
+
+  },
+
+  insert: function (arr, item, index) {
+
+  },
+
+  count: function (arr, item) {
+  },
+
+  duplicates: function (arr) {
+
+  },
+
+  square: function (arr) {
+
+  },
+
+  findAllOccurrences: function (arr, target) {
+
+  }
+};
