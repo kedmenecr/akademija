3359184a0ff934ed079e8863f34221f360d7e52b
Martina Safaric <msafaric@tria.hr>
2019-04-25 01:03:08 +0200

    array part is done

--- 19042019-js-test/MartinaSafaric/app/arrays.js
+++ 19042019-js-test/MartinaSafaric/app/arrays.js
@@ -1,59 +1,112 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
-  indexOf: function(arr, item) {
+	indexOf: function (arr, item) {
+		return arr.indexOf(item)
+	},
 
-  },
+	sum: function (arr) {
+		return arr.reduce((acc, item) => acc + item)
+	},
 
-  sum: function(arr) {
+	remove: function (arr, item) {
+		let newArr = []
+		for (let i = 0; i < arr.length; i++) {
+			if (arr[i] !== item) {
+				newArr.push(arr[i])
+			}
+		}
+		return newArr
+	},
 
-  },
+	removeWithoutCopy: function (arr, item) {
+		for (let i = 0; i < arr.length; i++) {
+			if (arr[i] === item) {
+				arr.splice(i, 1)
+				i--
+			}
+		}
 
-  remove: function(arr, item) {
+		return arr
+	},
 
-  },
+	append: function (arr, item) {
+		arr.push(item)
+		return arr
+	},
 
-  removeWithoutCopy: function(arr, item) {
+	truncate: function (arr) {
+		arr.pop()
+		return arr
+	},
 
-  },
+	prepend: function (arr, item) {
+		arr.unshift(item)
+		return arr
+	},
 
-  append: function(arr, item) {
+	curtail: function (arr) {
+		arr.shift()
+		return arr
+	},
 
-  },
+	concat: function (arr1, arr2) {
+		let result = arr1.concat(arr2)
+		return result
+	},
 
-  truncate: function(arr) {
+	insert: function (arr, item, index) {
+		arr.splice(index, 0, item)
+		return arr
+	},
 
-  },
+	count: function (arr, item) {
+		let res = []
 
-  prepend: function(arr, item) {
+		for (let i = 0; i < arr.length; i++) {
+			if (arr[i] === item) {
+				res.push(item)
+			}
+		}
 
-  },
+		return res.length
+	},
 
-  curtail: function(arr) {
+	duplicates: function (arr) {
+		let sortedArr = arr.sort()
+		let result = []
 
-  },
+		for (let i = 0; i < sortedArr.length; i++) {
+			if (sortedArr[i + 1] === sortedArr[i]) {
+				if (result.includes(sortedArr[i])) {
+					continue
+				} else {
+					result.push(sortedArr[i])
+				}
+			}
+		}
+		return result
+	},
 
-  concat: function(arr1, arr2) {
+	square: function (arr) {
+		let result = []
+		for (let i = 0; i < arr.length; i++) {
+			result.push(Math.pow(arr[i], 2))
+		}
+		return result
+	},
 
-  },
+	findAllOccurrences: function (arr, target) {
+		let result = []
+		let indexOfTarget = arr.indexOf(target)
 
-  insert: function(arr, item, index) {
+		for (let i = 0; i < arr.length; i++) {
+			if (indexOfTarget !== -1) {
+				result.push(indexOfTarget)
+				indexOfTarget = arr.indexOf(target, indexOfTarget + 1)
+			}
+		}
 
-  },
-
-  count: function(arr, item) {
-
-  },
-
-  duplicates: function(arr) {
-
-  },
-
-  square: function(arr) {
-
-  },
-
-  findAllOccurrences: function(arr, target) {
-
-  }
+		return result
+	}
 };
