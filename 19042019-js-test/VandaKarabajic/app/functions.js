c3677f45b4b121e9fa7ffd3753f504a22a6a6e91
Roksana <karabajic.vanda@gmail.com>
2019-05-16 10:00:14 +0200

    functions and logical operators

--- 19042019-js-test/VandaKarabajic/app/functions.js
+++ 19042019-js-test/VandaKarabajic/app/functions.js
@@ -1,39 +1,58 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.functionsAnswers = {
-  argsAsArray: function(fn, arr) {
+	argsAsArray: function (fn, arr) {
+		return fn(...arr);
+	},
 
-  },
+	speak: function (fn, obj) {
+		return fn.call(obj)
+	},
 
-  speak: function(fn, obj) {
+	functionFunction: function (str) {
+		return function (_str) {
+			return str + ", " + _str
+		}
+	},
 
-  },
+	makeClosures: function (arr, fn) {
 
-  functionFunction: function(str) {
+	},
 
-  },
+	partial: function (fn, str1, str2) {
+		return function (str3) {
+			return fn(str1, str2, str3)
+		}
+	},
 
-  makeClosures: function(arr, fn) {
+	useArguments: function () {
+		sums = 0;
+		for (let i = 0; i < arguments.length; i++) {
+			sums += arguments[i]
+		}
+		return sums
+	},
 
-  },
+	callIt: function (fn) {
+		var arr = [];
+		for (let i = 1; i < arguments.length; i++) {
+			arr.push(arguments[i])
+		}
+		return fn.apply(this, arr)
+		//cannot use "call" here cuz it requires the paramaters to be listed
+	},
 
-  partial: function(fn, str1, str2) {
+	partialUsingArguments: function (fn) {
 
-  },
+	},
 
-  useArguments: function() {
-
-  },
-
-  callIt: function(fn) {
-
-  },
-
-  partialUsingArguments: function(fn) {
-
-  },
-
-  curryIt: function(fn) {
-
-  }
+	curryIt: function (fn) {
+		return function (hello1) {
+			return function (hello2) {
+				return function (hello3) {
+					return fn.call(this, hello1, hello2, hello3);
+				}
+			}
+		}
+	}
 };
