d69f6d988fbc340512fd57093a88dba67bf723b3
Roksana <karabajic.vanda@gmail.com>
2019-05-31 09:04:17 +0200

    Added form for creating new items

--- 07_sencha_frontend/src/Home/AddNew.js
+++ 07_sencha_frontend/src/Home/AddNew.js
@@ -1,103 +1,78 @@
 import React, { Component } from "react";
 import {
-    FieldSet,
-    FormPanel, 
-    UrlField, 
-    TextField, 
-    EmailField, 
-    RadioField, 
-    TextAreaField, 
-    ToggleField, 
-    SliderField, 
-    DatePickerField, 
-    PasswordField,
-    SpinnerField,
-    CheckBoxField,
-    Button,
-    SelectField,
-    Container,
-    Label,
-    Toolbar
+	FieldSet,
+	FormPanel,
+	UrlField,
+	TextField,
+	EmailField,
+	RadioField,
+	TextAreaField,
+	ToggleField,
+	SliderField,
+	DatePickerField,
+	PasswordField,
+	SpinnerField,
+	CheckBoxField,
+	Button,
+	SelectField,
+	Container,
+	Label,
+	Toolbar
 } from "@sencha/ext-modern";
 
 import axios from "axios";
 import { small, medium } from "../responsiveFormulas";
 
-export default class Home extends Component {
- 
-  // store = new Ext.data.Store({
-  //   data: [this.state.todos]
-  // });
-  // async getAlert(data) {
-  //   const payload = {
-  //     lol: "lol2"
-  //   };
-  //   await axios.post(`http://localhost:9090/create`, payload).then(res => {
-  //     console.log(res);
-  //   });
-  // }
+export default class AddNew extends Component {
 
 
-    constructor() {
-        super();
-        this.state = {
-            // disabled: false,
-            categoryName: ''
-        };
-    }
+	state = {}
 
-    createNew = (event) => {
-        this.setState({categoryName: event.target.value})
-     
-        console.log("fdssdassa",this.state.categoryName)
-        console.log('NOICE')
-    }
-    // handleChange(event){
-    //     this.setState({
-    //         [event.target.type]: event.target.value
-    //       });
-    //     // this.setState({categoryName: event.target.categoryName})
-    // }
+	getState = () => {
+		console.log('this.state.categoryName', this.state.categoryName)
+	}
 
-    
-   
+	onChange = (field, value) => {
+		this.setState({ categoryName: value })
 
-    render() {
-        const { categoryName } = this.state
-        return (
-            <Container 
-                platformConfig={{
-                    phone: {
-                        layout: 'fit'
-                    },
-                    "!phone": {
-                        layout: 'center',
-                        padding: 10
-                    }
-                }}
-            >
-                <FormPanel 
-                    ref={form => this.form = form}
-                    shadow 
-                    padding="20"
-                    platformConfig={{
-                        "!phone": {
-                            maxHeight: 500,
-                            width: 350
-                        }
-                    }}
-                >
-                    <FieldSet ref="category" title="Add New Category" defaults={{labelAlign: "placeholder"}}>
-                        <TextField label="Category Name" required placeholder="This field is required" value={categoryName} onChange={this.createNew()}/>
-                       
-                    </FieldSet>
-                   
-                    <Toolbar shadow={false} docked="bottom" layout={{ type: 'hbox', pack: 'right' }}>
-                        <Button text={'Create'} margin="0 10 0 0" handler={this.createNew.bind(this)}/>
-                        <Button text="Reset" handler={() => this.form.cmp.reset()}/>
-                    </Toolbar>
-                </FormPanel>
-            </Container>
-        );
-    }
+	}
+
+	render() {
+		return (
+			<Container store={this.store}
+				platformConfig={{
+					phone: {
+						layout: 'fit'
+					},
+					"!phone": {
+						layout: 'center',
+						padding: 10
+					}
+				}}
+			>
+				<FormPanel
+					ref={form => this.form = form}
+					shadow
+					padding="20"
+					platformConfig={{
+						"!phone": {
+							maxHeight: 500,
+							width: 350
+						}
+					}}
+				>
+					<FieldSet title="Add New Category" defaults={{ labelAlign: "placeholder" }}>
+						<TextField
+							onChange={this.onChange}
+						/>
+					</FieldSet>
+
+					<Toolbar shadow={false} docked="bottom" layout={{ type: 'hbox', pack: 'right' }}>
+						<Button text={'Create'} margin="0 10 0 0" handler={this.getState} />
+						<Button text="Reset" handler={() => this.form.cmp.reset()} />
+					</Toolbar>
+				</FormPanel>
+			</Container>
+		);
+	}
 }
--- 07_sencha_frontend/src/Home/Home.js
+++ 07_sencha_frontend/src/Home/Home.js
@@ -1,116 +1,116 @@
 import React, { Component } from "react";
 import {
-  Grid,
-  Toolbar,
-  Column,
-  SearchField,
-  Button,
-  Image,
-  TextField
+	Grid,
+	Toolbar,
+	Column,
+	SearchField,
+	Button,
+	Image,
+	TextField
 } from "@sencha/ext-modern";
 
 //import { Panel } from '@sencha/ext-modern';
 //import { TextArea } from '@sencha/ext-modern';
 import axios from "axios";
-import data from "./data";
 import { small, medium } from "../responsiveFormulas";
 
 export default class Home extends Component {
-  state = {
-    todos: [],
-    todoTitles: [],
-    todoIDs: [],
-    todoStatuss: []
-  };
+	state = {
+		todos: [],
+		todoTitles: [],
+		todoIDs: [],
+		todoStatuss: []
+	};
 
 
-  // store = new Ext.data.Store({
-  //   data: [this.state.todos]
-  // });
-  // async getAlert(data) {
-  //   const payload = {
-  //     lol: "lol2"
-  //   };
-  //   await axios.post(`http://localhost:9090/create`, payload).then(res => {
-  //     console.log(res);
-  //   });
-  // }
+	store = new Ext.data.Store({
+		data: [this.state.todos]
+	});
+	// async getAlert(data) {
+	//   const payload = {
+	//     lol: "lol2"
+	//   };
+	//   await axios.post(`http://localhost:9090/create`, payload).then(res => {
+	//     console.log(res);
+	//   });
+	// }
 
-  async componentDidMount() {
-    // await axios.get(`https://jsonplaceholder.typicode.com/todos`)
-    await axios.get(`http://localhost:9090/data`).then(res => {
-      const todos = res.data;
-      this.setState({ todos });
-    });
+	async componentDidMount() {
+		// await axios.get(`https://jsonplaceholder.typicode.com/todos`)
+		await axios.get(`http://localhost:9090/data`).then(res => {
+			const todos = res.data;
 
-    this.store.add(this.state.todos);
-  }
+			this.setState({ todos });
+		});
 
-  render() {
-    return (
-      <Grid store={this.store}>
-        <Toolbar docked="top">
-          <SearchField
-            ui="faded"
-            ref={field => (this.query = field)}
-            placeholder="Search..."
-            onChange={this.onSearch.bind(this)}
-            responsiveConfig={{
-              [small]: {
-                flex: 1
-              },
-              [medium]: {
-                flex: undefined
-              }
-            }}
-          />
-          <Button
-            handler={() => this.getAlert()}
-            text="Add new item"
-            docked="right"
-            style={{ color: "red" }}
-          />
-        </Toolbar>
-        <Column
-          text="Category"
-          dataIndex="categoryName"
-          flex={2}
-          resizable
-          responsiveConfig={{
-            [small]: {
-              hidden: true
-            },
-            [medium]: {
-              hidden: false
-            }
-          }}
-        />
-        <Column
-          text="Product"
-          dataIndex="productName"
-          flex={2}
-          resizable
-          responsiveConfig={{
-            [small]: {
-              hidden: true
-            },
-            [medium]: {
-              hidden: false
-            }
-          }}
-        >
-          {this.state.todoTitles}
-        </Column>
-        <Column text="Price" dataIndex="price" flex={2} resizable />
-        <Column text="operations">
-          <TextField placeHolder="Enter Name..." label="Name" required />
-        </Column>
-      </Grid>
-    );
-  }
+		this.store.add(this.state.todos);
+	}
+
+	render() {
+		return (
+			<Grid store={this.store}>
+				<Toolbar docked="top">
+					<SearchField
+						ui="faded"
+						ref={field => (this.query = field)}
+						placeholder="Search..."
+						onChange={this.onSearch.bind(this)}
+						responsiveConfig={{
+							[small]: {
+								flex: 1
+							},
+							[medium]: {
+								flex: undefined
+							}
+						}}
+					/>
+					<Button
+						handler={() => this.getAlert()}
+						text="Add new item"
+						docked="right"
+						style={{ color: "red" }}
+					/>
+				</Toolbar>
+				<Column
+					text="Category"
+					dataIndex="categoryName"
+					flex={2}
+					resizable
+					responsiveConfig={{
+						[small]: {
+							hidden: true
+						},
+						[medium]: {
+							hidden: false
+						}
+					}}
+				/>
+				<Column
+					text="Product"
+					dataIndex="productName"
+					flex={2}
+					resizable
+					responsiveConfig={{
+						[small]: {
+							hidden: true
+						},
+						[medium]: {
+							hidden: false
+						}
+					}}
+				>
+					{this.state.todoTitles}
+				</Column>
+				<Column text="Price" dataIndex="price" flex={2} resizable />
+				<Column text="operations">
+					<TextField placeHolder="Enter Name..." label="Name" required />
+				</Column>
+			</Grid>
+		);
+	}
 
   /**
    * Filter the store when the user types in the search box
    */
-  onSearch = () => {};
+	onSearch = () => { };
 }
