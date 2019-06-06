2dc42f3f03a8757c644c2700634326e06060e702
Marko <mmustac@tria.hr>
2019-06-06 19:27:33 +0200

    service chain

--- 14_ms_deploy/06_mmustac/index.js
+++ 14_ms_deploy/06_mmustac/index.js
@@ -6,15 +6,18 @@ const request = require('request');
 const port = 9096;
 const app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
 
 const serviceToCall = 'http://35.234.76.207:9097/';
 //e
 app.post("/akademija/:id", (req, res) => {
-    const parametar = req.param.id;
+    const parametar = req.params.id;
     if (parametar && typeof parametar === "string" && parametar[parametar.length - 1] === "e") {
         request.post({
             url: `${serviceToCall}/akademija/${parametar}f`
         })
+        res.send({ response: "Poslano prema: " + serviceToCall + "akademija/abcdef" })
     } else {
         res.status(400).send({ errorMessage: "Servis nije zadovoljio uvjete. Za dodatna pitanja obratite se Marku Mustaču, Hvala!!" });
     }
